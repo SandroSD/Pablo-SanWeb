3157f2236479066fa5753a7c6bbbc7fa8b3e8265
Pablo Ratibel <pabloratibel@gmail.com>
2017-11-06 00:03:43 -0300

    cambio de fondo y redaccion de contenido

--- index.php
+++ index.php
@@ -13,36 +13,42 @@
 
 <div class="bgimg-1">
   <div class="caption">
-    <span class="border">LOGO - NOMBRE COMERCIAL</span>
+    <span class="border">SMART SOCKET</span>
   </div>
 </div>
 
 <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
   <h3 style="text-align:center;">¿Quiénes Somos?</h3>
-  <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+  <p>Somos un grupo de estudiantes avanzados de la carrera de Ingeniería Informatica trabajando juntos. 
+  
+  </p>
 </div>
 
 <div class="bgimg-2">
   <div class="caption">
-    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">NUESTROS SERVICIOS</span>
   </div>
 </div>
 
 <div style="position:relative;">
   <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-    <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+    <p>Construimos soluciones a medida para maximizar el valor al producto. Para nosotros, cada proyecto es un desafío y un compromiso.</p>
+    <p>La WEB nos importa. Nos enfocamos en construir aplicaciones escalables en la nube.</p>
+    <p>Mejoramos Experiencias. Actualizamos sistemas y nos enfocamos en crear ambientes de trabajo amigables y seguros.</p>
+
   </div>
 </div>
 
 <div class="bgimg-3">
   <div class="caption">
-    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">SCROLL UP</span>
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">NUESTRO EQUIPO</span>
   </div>
 </div>
 
 <div style="position:relative;">
   <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-    <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+    <p>Sandro Dezerio. DESARROLLADOR WEB. Futuro ingeniero. Su experiencia en desarrollo web lo hace invencible. Sandro tiene interés especial por compartir su conocimiento a través de blogs, artículos y redes sociales. En su tiempo libre, disfruta ver series de televisión, y hacer cursos online.<p>
+    <p>Pablo Ratibel. PROGRAMADOR NERD. Ingeniero en proceso. Pablo es un entuciasta de trabajar en muchas cosas, dicta clases en secundaria, mantiene sistemas y disfruta dando cursos de programacion para olimpiadas. En su tiempo libre, adora hacer viajes cortos con amigos y jugar al futbol.</p>
   </div>
 </div>
 
