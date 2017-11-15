a1da4f69232e58edd265a51bd501d59f38d71a62
Pablo Ratibel <pabloratibel@gmail.com>
2017-11-15 16:10:24 -0300

    Cambio de Lugar texto servicios

--- index.php
+++ index.php
@@ -35,11 +35,6 @@
 
 <div class="container-fluid" style="position:relative;">
   <div class="row" id="contServicios">
-    <div class="col-md-12" id="tituloServicios" style="text-align: justify;">
-    <p>Brindamos servicio personalizado, desarrollando la solución que mejor satisfaga las necesidades de cada proyecto.
-  Ofrecemos servicios completos de Diseño de sitios Web, actualizacion y mantenimiento de sistemas informaticos y 
-  desarrollo de sistemas. Nos caracterizamos por la creación de aplicaciones y sitios web auto administrables con sistemas de gestión veloces, intuitivos y fáciles de utilizar.</p>
-    </div>
     <div class="col-md-1"></div>
     <div class="col-md-5" id="izqServicios">
       <h3> Mejoramos Experiencias </h3>
@@ -50,6 +45,11 @@
       <h3> Generamos Arte Informático</h3>
       <img src="img/desarrollo_web.png"  id="desarrolloImg">
       <p>Nos enfocamos en construir aplicaciones escalables en la nube.</p>
+    </div>
+    <div class="col-md-12" id="tituloServicios" style="text-align: justify;">
+    <p>Brindamos servicio personalizado, desarrollando la solución que mejor satisfaga las necesidades de cada proyecto.
+  Ofrecemos servicios completos de Diseño de sitios Web, actualizacion y mantenimiento de sistemas informaticos y 
+  desarrollo de sistemas. Nos caracterizamos por la creación de aplicaciones y sitios web auto administrables con sistemas de gestión veloces, intuitivos y fáciles de utilizar.</p>
     </div>
   </div>
 </div>
